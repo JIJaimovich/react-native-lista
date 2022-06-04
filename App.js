2ee14260d98ff4401c08132564d49810985daf0a
Juan Ignacio <jaimovich.ignacio@gmail.com>
2022-06-04 19:40:46 -0300

    lista optimizada

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { themes } from "./src/constants/themes/index";
+import { List, Modal } from "./src/components/index";
+
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [itemSelected, setItemSelected] = useState({});
+
+  const onHandleInput = (text) => {
+    setTask(text);
+  };
+
+  const onHandleSubmit = () => {
+    setTasks((currentTasks) => [
+      ...currentTasks,
+      { id: Math.random(), value: task },
+    ]);
+    setTask("");
+  };
+
+  const onHandleDelete = (itemSelected) => {
+    setTasks((currentTasks) =>
+      currentTasks.filter((task) => task.id !== itemSelected.id)
+    );
+    setItemSelected({});
+    setModalVisible(!modalVisible);
+  };
+
+  const handleModal = (id) => {
+    setItemSelected(tasks.filter((item) => item.id === id)[0]);
+    setModalVisible(!modalVisible);
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={themes.container}>
+      <View style={themes.containerTask}>
+        <TextInput
+          style={themes.textInput}
+          placeholder="Crear una nota nueva"
+          value={task}
+          onChangeText={onHandleInput}
+        />
+        <Button
+          title="Crear"
+          onPress={() => onHandleSubmit()}
+          disabled={task.length === 0}
+        />
+      </View>
+      <List 
+        tasks={tasks}
+        onPressItem={handleModal}
+      />
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        onRequestClose={() => null}
+      >
+        <View style={themes.modalContent}>
+          <View style={themes.modalTitleContainer}>
+            <Text>Eliminar nota</Text>
+            <TouchableOpacity
+              style={themes.deleteButton}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={themes.deleteButtonText}>X</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={themes.modalText}>¿Está seguro?</Text>
+          <Text style={themes.modalMessage}>{itemSelected.value}</Text>
+
+          <Button title="Okay" onPress={() => onHandleDelete(itemSelected)} />
+        </View>
+      </Modal>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/constants/themes/index.js
@@ -0,0 +1,62 @@
+export const themes = {
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  textInput: {
+    borderColor: "#465C69",
+    borderBottomWidth: 1,
+    marginBottom: 10,
+    width: "60%",
+    height: 40,
+    fontSize: 14,
+    color: "#212121",
+  },
+  containerTask: {
+    marginTop: 40,
+    paddingHorizontal: 25,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  modalContent: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    alignItems: "center",
+  },
+  modalTitleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 20,
+    marginHorizontal: 20,
+  },
+  modalText: {
+    fontSize: 16,
+    marginVertical: 10,
+  },
+  modalMessage: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginVertical: 10,
+    
+  },
+  deleteButton: {
+    backgroundColor: "#465C69",
+    margin: 10,
+    height: 20, 
+    paddingHorizontal: 8,
+    borderRadius: 10,
+  },
+  deleteButtonText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+};
